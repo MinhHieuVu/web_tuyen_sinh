3b5a48318685c0acf5fbeb44af683e7392c05e7f
MinhHieuVu <minhhieuvux@gmail.com>
2022-06-27 16:02:42 +0700

    update form page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
              path_names: { sign_in: 'dang-nhap', sign_out: 'dang-xuat', sign_up: 'dang-ky' }
 
   root to: "home#index"
+  get ':school_id', to: 'home#index'
 
   namespace :admin do
     resources :school_infos
