9c210dbac18965327295802163b620f84da35ea7
MinhHieuVu <minhhieuvux@gmail.com>
2022-07-03 10:23:26 +0700

    update form admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
   def index
     @provinces = Province.all
+    @schools = School.all
   end
   def school_index
     @schools = School.all
+    @provinces = Province.all
   end
+
+  def school_info_index
+    @schools = School.all
+    @school_infos = SchoolInfo.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
              path_names: { sign_in: 'dang-nhap', sign_out: 'dang-xuat', sign_up: 'dang-ky' }
 
   root to: "home#index"
-  get 'home/index', as: 'school_index'
+  get ':school_id',to: 'home#index' , as: 'school_index'
+  get ':school_id/:school_info_id' , to: 'home#index', as: "school_info_index"
+
 
   namespace :admin do
     resources :school_infos
