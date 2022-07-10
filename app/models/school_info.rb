b753970758cfa52ef760a3b12902b2bacb12a4bf
MinhHieuVu <minhhieuvux@gmail.com>
2022-07-10 13:38:18 +0700

    update form

--- app/models/school_info.rb
+++ app/models/school_info.rb
@@ -2,6 +2,6 @@ class SchoolInfo < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  belongs_to :school, optional: true, touch: true
+  belongs_to :school, optional: true
 
 end
